b5f438382910cac5a465bca9a06fe8a2166aa84e
prashantpic <prashantpic001@gmail.com>
2025-05-23 16:18:23 +0000

    Files added by TheSSS-AI

---
+++  Backend.Core.Module/.prettierrc.js
@@ -0,0 +1,13 @@
+```javascript
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+};
+```
